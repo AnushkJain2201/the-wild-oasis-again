26449c8a332779d21646f02ca6c8eb030979ff5e
Anushk Jain <anushk2201@gmail.com>
2025-04-30 10:37:33 +0530

    feat(ui): add Row component and integrate into App Introduce a new Row component to manage layout with horizontal and vertical orientations. Integrate Row into App to improve structure and spacing. Additionally, update Button component to use default props for size and variation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,53 @@
-import styled from "styled-components"
-import GlobalStyles from "../styles/GlobalStyles"
+import styled from "styled-components";
+import GlobalStyles from "../styles/GlobalStyles";
 
-import Button from "../ui/Button"
-import Input from "../ui/Input"
-import Heading from "../ui/Heading"
+import Button from "../ui/Button";
+import Input from "../ui/Input";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20px;
-`
+	padding: 20px;
+`;
 
 function App() {
+	return (
+		<>
+			<GlobalStyles />
+			<StyledApp>
+				<Row>
+					<Row type='horizontal'>
+						<Heading as='h1'>The Wild Oasis</Heading>
 
-  return (
-    <>
-    <GlobalStyles />
-    <StyledApp>
-      <Heading as='h1'>The Wild Oasis</Heading>
-      <Heading as='h2'>Check In / Check Out</Heading>
-      <Heading as='h3'>Forms</Heading>
-      <Button onClick={() => alert("Hiiiiiiiiii")}>Check In</Button>
-      <Button onClick={() => alert("Byeeeeeeeee")}>Check Out</Button>
+						<div>
+							<Heading as='h2'>Check In / Check Out</Heading>
+							<Button onClick={() => alert("Hiiiiiiiiii")}>
+								Check In
+							</Button>
+							<Button variation='secondary' size='medium' onClick={() => alert("Byeeeeeeeee")}>
+								Check Out
+							</Button>
+						</div>
+					</Row>
 
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
-    </>
-  )
+					<Row>
+						<Heading as='h3'>Forms</Heading>
+
+						<form>
+							<Input
+								type='number'
+								placeholder='Number of guests'
+							/>
+							<Input
+								type='number'
+								placeholder='Number of guests'
+							/>
+						</form>
+					</Row>
+				</Row>
+			</StyledApp>
+		</>
+	);
 }
 
-export default App
+export default App;
