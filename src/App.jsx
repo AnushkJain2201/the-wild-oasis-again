7aa98c4b6331f3238a1b1715d3b6b31e1111244d
Anushk Jain <anushk2201@gmail.com>
2025-04-30 11:45:16 +0530

    feat: add app layout with header, sidebar, and main content
    Introduce a new AppLayout component that includes a header, sidebar, and main content area. This layout is applied to all routes within the application to ensure a consistent structure and improve user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Account from "../pages/Account";
 import Login from "../pages/Login";
 import PageNotFound from "../pages/PageNotFound";
 import GlobalStyles from "../styles/GlobalStyles";
+import AppLayout from "../ui/AppLayout";
 
 const App = () => {
 	return (
@@ -15,15 +16,18 @@ const App = () => {
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
-					<Route
-						index
-						element={<Navigate replace to='dashboard' />}
-					/>
-					<Route path='dashboard' element={<Dashboard />} />
-					<Route path='bookings' element={<Cabins />} />
-					<Route path='users' element={<Users />} />
-					<Route path='settings' element={<Sttings />} />
-					<Route path='account' element={<Account />} />
+					<Route element={<AppLayout />}>
+						<Route
+							index
+							element={<Navigate replace to='dashboard' />}
+						/>
+						<Route path='dashboard' element={<Dashboard />} />
+						<Route path='bookings' element={<Cabins />} />
+						<Route path='users' element={<Users />} />
+						<Route path='settings' element={<Sttings />} />
+						<Route path='account' element={<Account />} />
+					</Route>
+
 					<Route path='login' element={<Login />} />
 					<Route path='*' element={<PageNotFound />} />
 				</Routes>
