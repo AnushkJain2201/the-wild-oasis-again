3b7e686c2992f0c8f16e350682147b7b3ff06f19
Anushk Jain <anushk2201@gmail.com>
2025-05-02 21:17:48 +0530

    feat: integrate React Query for state management
    Add @tanstack/react-query and @tanstack/react-query-devtools dependencies to improve data fetching and state management. Wrap the application with QueryClientProvider to enable caching and configure default query options for better performance.

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,22 @@ import Login from "../pages/Login";
 import PageNotFound from "../pages/PageNotFound";
 import GlobalStyles from "../styles/GlobalStyles";
 import AppLayout from "../ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// It sets up the cache before the client
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 60 * 1000,
+		},
+	},
+});
 
 const App = () => {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
@@ -34,7 +46,7 @@ const App = () => {
 					<Route path='*' element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
-		</>
+		</QueryClientProvider>
 	);
 };
 
