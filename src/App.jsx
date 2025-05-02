05abdd344d8a05a322df6e33ee6cd1c5e3ba027b
Anushk Jain <anushk2201@gmail.com>
2025-05-02 19:42:47 +0530

    feat: integrate Supabase for cabins data management
    Add Supabase dependency and implement API services for fetching cabins data. Update routing to correctly map the cabins page and display cabin images from Supabase storage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Dashboard from "../pages/Dashboard";
+import Bookings from "../pages/Bookings";
 import Cabins from "../pages/Cabins";
 import Users from "../pages/Users";
 import Sttings from "../pages/Settings";
@@ -22,7 +23,8 @@ const App = () => {
 							element={<Navigate replace to='dashboard' />}
 						/>
 						<Route path='dashboard' element={<Dashboard />} />
-						<Route path='bookings' element={<Cabins />} />
+						<Route path='cabins' element={<Cabins />} />
+						<Route path='bookings' element={<Bookings />} />
 						<Route path='users' element={<Users />} />
 						<Route path='settings' element={<Sttings />} />
 						<Route path='account' element={<Account />} />
