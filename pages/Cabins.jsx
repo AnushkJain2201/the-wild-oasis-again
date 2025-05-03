3548da47bf548d2b7e0ee047d2ae19d169dc5032
Anushk Jain <anushk2201@gmail.com>
2025-05-03 11:28:23 +0530

    feat(cabins): add cabin table and row components
    Implement CabinTable and CabinRow components to display cabin data fetched from the API. Added date-fns dependency for date manipulation and updated helper functions to use the new dependency. Removed unused imports and simplified the Cabins page to render the new components.

--- pages/Cabins.jsx
+++ pages/Cabins.jsx
@@ -1,21 +1,20 @@
-import { useEffect } from "react";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
-  useEffect(() => {
-    getCabins().then((data) => {
-      console.log(data);
-    });
-  }, [])
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://nowepdlshtpdzrsjpvhy.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg" alt="dfdf" />
-    </Row>
-  );
+	return (
+		<>
+			<Row type='horizontal'>
+				<Heading as='h1'>All cabins</Heading>
+				<p>Filter/Sort</p>
+			</Row>
+
+			<Row>
+				<CabinTable />
+			</Row>
+		</>
+	);
 }
 
 export default Cabins;
